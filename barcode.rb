0daa06c75e3c008b68855510d6f470eded605cff
Tyler Standridge <tstand90@gmail.com>
2014-01-13 16:04:44 -0800

    Code128 does not do last character

--- barcode.rb
+++ barcode.rb
@@ -33,7 +33,7 @@ class Code39
     barcode_string.scan(/.{12}/).each do |s|
   	  decoded_string += CODE39.invert[s]
     end
-    puts decoded_string
+    decoded_string
   end
 
   def draw(width, height, barcode_string, ratio=2, border=5)
@@ -114,20 +114,77 @@ class Code128
     105 => '11010011100', 106 => '1100011101011'
   }
 
+  MAP_B = {
+    ' ' => 0, '!' => 1, '"' => 2, '#' => 3, '$' => 4, '%' => 5, '&' => 6,
+    "'" => 7, '(' => 8, ')' => 9, '*' => 10, '+' => 11, ',' => 12, '-' => 13,
+    '.' => 14, '/' => 15, '0' => 16, '1' => 17, '2' => 18, '3' => 19,
+    '4' => 20, '5' => 21, '6' => 22, '7' => 23, '8' => 24, '9' => 25,
+    ':' => 26, ';' => 27, '<' => 28, '=' => 29, '>' => 30, '?' => 31,
+    '@' => 32, 'A' => 33, 'B' => 34, 'C' => 35, 'D' => 36, 'E' => 37,
+    'F' => 38, 'G' => 39, 'H' => 40, 'I' => 41, 'J' => 42, 'K' => 43,
+    'L' => 44, 'M' => 45, 'N' => 46, 'O' => 47, 'P' => 48, 'Q' => 49,
+    'R' => 50, 'S' => 51, 'T' => 52, 'U' => 53, 'V' => 54, 'W' => 55,
+    'X' => 56, 'Y' => 57, 'Z' => 58, '[' => 59, '\\' => 60, ']' => 61,
+    '^' => 62, '_' => 63, '`' => 64, 'a' => 65, 'b' => 66, 'c' => 67,
+    'd' => 68, 'e' => 69, 'f' => 70, 'g' => 71, 'h' => 72, 'i' => 73,
+    'j' => 74, 'k' => 75, 'l' => 76, 'm' => 77, 'n' => 78, 'o' => 79,
+    'p' => 80, 'q' => 81, 'r' => 82, 's' => 83, 't' => 84, 'u' => 85,
+    'v' => 86, 'w' => 87, 'x' => 88, 'y' => 89, 'z' => 90, '{' => 91,
+    '|' => 92, '}' => 93, '~' => 94
+  }
+
   def encode(barcode_string)
-    encoded_string = CODE128[103]     # Start code
+    puts barcode_string
+    encoded_string = CODE128[104]     # Start code
     barcode_string.each_char do |s|
-      encoded_string += CODE128[s.upcase]
+      encoded_string += CODE128[MAP_B[s]]
     end
     encoded_string += CODE128[106]    # End code
+    puts encoded_string
     encoded_string
   end
 
   def decode(barcode_string)
     decoded_string = ''
-    barcode_string.scan(/.{12}/).each do |s|
+    barcode_string.each_char do |s|
       decoded_string += CODE128.invert[s]
     end
-    puts decoded_string
+    decoded_string
+  end
+
+  def draw(width, height, barcode_string, ratio=2, border=20)
+    encoded_string = encode(barcode_string)
+
+    if ratio == 2
+      size = (border * 2) + encoded_string.size
+    end
+
+    puts size
+
+    image = ChunkyPNG::Image.new(size, height, ChunkyPNG::Color::WHITE)
+    x = border
+=begin
+    encoded_string.each_char do |character|
+      character.each_char do |bit|
+        if bit == '1'
+          (0..height-1).each do |y|
+            image[x, y] = ChunkyPNG::Color::BLACK
+          end
+        end
+        x += 1
+      end
+    end
+    image.save('barcode128.png')
+  end
+=end
+    encoded_string.each_char do |character|
+      if character == '1'
+        (0..height-1).each do |y|
+          image[x, y] = ChunkyPNG::Color::BLACK
+        end
+      end
+      x += 1
+    end
+    image.save('barcode128.png')
   end
 end
